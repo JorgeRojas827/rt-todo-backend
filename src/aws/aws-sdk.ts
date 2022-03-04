a86743b5f615f88872a884604d2cf60ad51337c4
JorgeRojas827 <jorgerojaspoma09@gmail.com>
2022-03-03 19:18:57 -0500

    ➕ ADD: reporte-aws-s3

--- src/aws/aws-sdk.ts
+++ src/aws/aws-sdk.ts
@@ -1,8 +1,9 @@
 import {S3} from '@aws-sdk/client-s3';
+require('dotenv').config()
 
 const s3Client= new S3({region: 'us-east-1', credentials: {
-    accessKeyId: 'AKIA46CZA5EPN5SFTZEW',
-    secretAccessKey: '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'
+    accessKeyId: process.env.ACCESSKEY!,
+    secretAccessKey: process.env.SECRETKEY!
 }});
 
 export { s3Client };
